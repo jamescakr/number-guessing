125c7210afdefe268416275aa12a2c6190b82dbc
jamesjo <jamescakr@gmail.com>
2024-06-30 11:50:50 -0600

    second commit

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 let randomNum = 0;
 let playButton = document.getElementById("play-button");
-playButton.addEventListener("click", play);
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
+
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", () => (userInput.value = ""));
 
 function pickRandomNum() {
   randomNum = Math.floor(Math.random() * 100) + 1;
@@ -11,12 +19,49 @@ function pickRandomNum() {
 
 function play() {
   let userValue = userInput.value;
+
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "number has to be between 1 and 100";
+    return;
+  }
+
+  if (history.includes(userValue)) {
+    resultArea.textContent = "Same number detected, pick another number";
+    return;
+  }
+  chances--;
+  chanceArea.textContent = `you have ${chances} chances left`;
+
   if (userValue < randomNum) {
     resultArea.textContent = "Up";
   } else if (userValue > randomNum) {
     resultArea.textContent = "Down";
   } else {
     resultArea.textContent = "Congrats!";
+    chanceArea.textContent = "";
+    gameOver = true;
+  }
+
+  history.push(userValue);
+  console.log(history);
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
   }
 }
+
+function reset() {
+  userInput.value = "";
+  pickRandomNum();
+  playButton.disabled = false;
+
+  chances = 5;
+  chanceArea.textContent = `you have ${chances} chances left`;
+  history = [];
+}
+
 pickRandomNum();
